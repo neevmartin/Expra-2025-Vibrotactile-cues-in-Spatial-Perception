966c09855691ebcd931cb225f025f1f77da6c481
moricetamol <gerhardt.mor@gmail.com>
2025-06-03 15:20:30 +0200

    Refactor participant folder creation and update input parameter handling

--- Experiment_Code/main.py
+++ Experiment_Code/main.py
@@ -1,10 +1,22 @@
 from experiment import VibrotactileCueExperiment
 from config_loader import ExperimentConfig
 from vibration_controller import VibrationController
+from Experimental_Setup import Experimental_Setup
+from psychopy.visual import Window
+import os
 
-def create_participant_folder():
-    ...
-    return 'some_folder'
+def create_participant_folder(participant_id: str) -> str:
+    '''Function to create a participant folder.'''
+    main_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(main_dir, 'data')
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+        
+    participant_dir = os.path.join(data_dir, participant_id)
+    if not os.path.exists(participant_dir):
+        os.makedirs(participant_dir)    
+    
+    return participant_dir
 
 def input_params():
     '''Function to handle param input.'''
@@ -16,7 +28,7 @@ def input_params():
         'screenID'           : input('Screen: '),
 
         'participantID'      : input('Participant ID: '),
-        'participant_dir'    : create_participant_folder(),
+        #'participant_dir'    : create_participant_folder(),
 
         'mode'               : input('Mode: '),
         'mapping'            : input('Mapping: '),
@@ -24,6 +36,9 @@ def input_params():
 
         'debug'              : input('Debug: ')
     })
+    
+    participant_dir = create_participant_folder(params['participantID'])
+    params['participant_dir'] = participant_dir
 
     return params
 
@@ -39,7 +54,7 @@ def input_params_example():
         'screenID'           : 0,
 
         'participantID'      : 'abc',
-        'participant_dir'    : create_participant_folder(),
+        #'participant_dir'    : create_participant_folder(),
 
         'mode'               : 'reaching',
         'mapping'            : 'direct',
@@ -47,12 +62,45 @@ def input_params_example():
 
         'debug'              : True
     })
+    participant_dir = create_participant_folder(params['participantID'])
+    params['participant_dir'] = participant_dir
 
+    return params
+
+def new_input_params(setup_data: dict) -> dict:
+    params = {}
+    
+    params.update({
+        "windowed": False,
+        "resolution": [1440,900],
+        "screenID": 0,
+        "participantID": setup_data['participantID'],
+        "participant_dir": create_participant_folder(setup_data['participantID']),
+        "mode": setup_data['mode'],
+        "mapping": "direct",
+        "config_dir": "configs",
+        "debug": True
+    })
+    
     return params
 
 def main():
     '''Starts the experiment.'''
-    params =  input_params_example()
+    #params =  input_params()
+    window = Window(
+        size=[960, 540],
+        screen=0,
+        fullscr=False,
+        units='pix',
+        colorSpace='rgb255',
+        color=[128, 128, 128],
+    )
+    setup = Experimental_Setup(
+        window
+    )
+    setup_params = setup.run()
+    params = new_input_params(setup_params)
+    window.close()
 
     win_config = {key : params.get(key) for key in [
         'windowed',
@@ -70,22 +118,22 @@ def main():
     ]}
 
     # Use when Arduino board is connected. 
-    #
-    # vibration_controller = VibrationController(testing=False)
-    # experiment = VibrotactileCueExperiment(
-    #     win_config=win_config,
-    #     experiment_config=experiment_config,
-    #     participant=participant,
-    #     vibration_controller=vibration_controller
-    #     debug = params['debug']
-    # )
-
+    
+    vibration_controller = VibrationController(testing=True)
     experiment = VibrotactileCueExperiment(
         win_config=win_config,
         experiment_config=experiment_config,
         participant=participant,
+        vibration_controller=vibration_controller,
         debug = params['debug']
     )
+
+    # experiment = VibrotactileCueExperiment(
+    #     win_config=win_config,
+    #     experiment_config=experiment_config,
+    #     participant=participant,
+    #     debug = params['debug']
+    # )
 
     experiment.run()
 
