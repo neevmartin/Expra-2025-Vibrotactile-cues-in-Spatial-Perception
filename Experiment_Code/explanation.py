259fac04866f1f6f2a834f945b58bce359bc86d0
bpminh1 <156366544+bpminh1@users.noreply.github.com>
2025-06-05 15:42:48 +0200

    Add explanation texts

---
+++ Experiment_Code/explanation.py
@@ -0,0 +1,9 @@
+from enum import Enum
+class Explanation(Enum):
+    INTRODUCTION = ["text_slides/Folie1.JPG", "text_slides/Folie2.JPG", "text_slides/Folie3.JPG"]
+    TUTORIAL = ["text_slides/Folie4.JPG"]
+    REACHING = ["text_slides/Folie5.JPG", "text_slides/Folie6.JPG", "text_slides/Folie7.JPG", "text_slides/Folie8.JPG",
+                "text_slides/Folie9.JPG", "text_slides/Folie10.JPG"]
+    AVOIDING = ["text_slides/Folie11.JPG", "text_slides/Folie12.JPG", "text_slides/Folie13.JPG", "text_slides/Folie14.JPG",
+                "text_slides/Folie15.JPG", "text_slides/Folie16.JPG"]
+    BREAK = ["text_slides/Folie17.JPG", "text_slides/Folie18.JPG", "text_slides/Folie19.JPG", "text_slides/Folie20.JPG"]
