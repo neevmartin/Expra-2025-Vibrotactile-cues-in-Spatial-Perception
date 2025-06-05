e8ec2f297b84458c6a36c011c94456d50ed7bba8
bpminh1 <156366544+bpminh1@users.noreply.github.com>
2025-06-05 22:34:31 +0200

    Split phases

--- Experiment_Code/explanation.py
+++ Experiment_Code/explanation.py
@@ -2,8 +2,17 @@ from enum import Enum
 class Explanation(Enum):
     INTRODUCTION = ["text_slides/Folie1.JPG", "text_slides/Folie2.JPG", "text_slides/Folie3.JPG"]
     TUTORIAL = ["text_slides/Folie4.JPG"]
-    REACHING = ["text_slides/Folie5.JPG", "text_slides/Folie6.JPG", "text_slides/Folie7.JPG", "text_slides/Folie8.JPG",
-                "text_slides/Folie9.JPG", "text_slides/Folie10.JPG"]
-    AVOIDING = ["text_slides/Folie11.JPG", "text_slides/Folie12.JPG", "text_slides/Folie13.JPG", "text_slides/Folie14.JPG",
-                "text_slides/Folie15.JPG", "text_slides/Folie16.JPG"]
+
+    PRETEST_REACHING = ["text_slides/Folie5.JPG", "text_slides/Folie6.JPG"]
+    TRAINING_REACHING = ["text_slides/Folie7.JPG"]
+    POSTTEST_REACHING1 = ["text_slides/Folie8.JPG"]
+    RECAP_REACHING = ["text_slides/Folie9.JPG"]
+    POSTTEST_REACHING2 = ["text_slides/Folie10.JPG"]
+
+    PRETEST_AVOIDING = ["text_slides/Folie11.JPG", "text_slides/Folie12.JPG"]
+    TRAINING_AVOIDING = ["text_slides/Folie13.JPG"]
+    POSTTEST_AVOIDING1 = ["text_slides/Folie14.JPG"]
+    RECAP_AVOIDING = ["text_slides/Folie15.JPG"]
+    POSTTEST_AVOIDING2 = ["text_slides/Folie16.JPG"]
+
     BREAK = ["text_slides/Folie17.JPG", "text_slides/Folie18.JPG", "text_slides/Folie19.JPG", "text_slides/Folie20.JPG"]
